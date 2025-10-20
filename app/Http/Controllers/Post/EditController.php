845d869996e71d1b9fefeee5aec87a0612bf3aa4
rendleks <0rendl050@gmail.com>
2025-10-20 23:26:12 +0300

    add class: factory and seed, generate random values

--- app/Http/Controllers/Post/EditController.php
+++ app/Http/Controllers/Post/EditController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\Tag;
 
 class EditController extends BaseController
 {
-    public function __invoke($post)
+    public function __invoke(Post $post)
     {
         $categories = Category::all();
         $tags = Tag::all();
